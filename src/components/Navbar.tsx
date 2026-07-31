fbc2e69894fc9b55c9e4bf81df32de741f72bfba
tavaresnk7-lgtm <tavares.nk7@gmail.com>
2026-07-30 23:48:50 -0300

    Optical vertical alignment for logo in header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-[80px]">
           {/* Logo */}
-          <a href="#hero" className="shrink-0 flex items-center">
+          <a href="#hero" className="shrink-0 flex items-center translate-y-1">
             <Logo size="lg" />
           </a>
 
